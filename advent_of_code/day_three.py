87315bfa3598378997c8b2fdbb9e89de7b99b289
luisdibdin <luisdibdin@icloud.com>
2023-12-14 17:02:31 +0000

    started day 3

--- advent_of_code/day_three.py
+++ advent_of_code/day_three.py
@@ -19,6 +19,35 @@ def get_position_of_symbols_in_row(row: str) -> list[int]:
     return indices
 
 
+def get_digit_coordinates(
+    matrix: list[str], indices: list[tuple[int, int]]
+) -> list[tuple[int, int]]:
+    return [(i, j) for i, j in indices if matrix[i][j].isdigit()]
+
+
+def generate_all_adjacent_coordinates(
+    indices: list[tuple[int, int]], max_index: tuple[int, int]
+) -> list[tuple[int, int]]:
+    return [i for j in indices for i in generate_adjacent_coordinates(j, max_index)]
+
+
+def generate_adjacent_coordinates(
+    index: tuple[int, int], max_index: tuple[int, int]
+) -> list[tuple[int, int]]:
+    indices = [
+        (i, j)
+        for i in range(index[0] - 1, index[0] + 2)
+        for j in range(index[1] - 1, index[1] + 2)
+        if 0 <= i <= max_index[0] and 0 <= j <= max_index[1]
+    ]
+
+    return indices
+
+
 if __name__ == "__main__":
     puzzle_txt = load_txt("advent_of_code/files/gear_ratios.txt")
-    print(get_position_of_symbols_in_matrix(puzzle_txt))
+    max_index = (len(puzzle_txt), len(puzzle_txt[0]))
+    symbol_indices = get_position_of_symbols_in_matrix(puzzle_txt)
+    adjacent_indices = generate_all_adjacent_coordinates(symbol_indices, max_index)
+    digit_indices = get_digit_coordinates(puzzle_txt, adjacent_indices)
+    print(digit_indices)
--- tests/test_day_three.py
+++ tests/test_day_three.py
@@ -1,6 +1,9 @@
 from advent_of_code.day_three import (
     get_position_of_symbols_in_row,
     get_position_of_symbols_in_matrix,
+    generate_adjacent_coordinates,
+    generate_all_adjacent_coordinates,
+    get_digit_coordinates,
 )
 
 
@@ -18,3 +21,52 @@ def test_get_position_of_symbols_in_matrix():
         (1, 1),
         (1, 4),
     ]
+
+
+def test_generate_adjacent_coordinates():
+    assert generate_adjacent_coordinates((1, 4), (6, 6)) == [
+        (0, 3),
+        (0, 4),
+        (0, 5),
+        (1, 3),
+        (1, 4),
+        (1, 5),
+        (2, 3),
+        (2, 4),
+        (2, 5),
+    ]
+    assert generate_adjacent_coordinates((0, 0), (6, 6)) == [
+        (0, 0),
+        (0, 1),
+        (1, 0),
+        (1, 1),
+    ]
+    assert generate_adjacent_coordinates((6, 6), (6, 7)) == [
+        (5, 5),
+        (5, 6),
+        (5, 7),
+        (6, 5),
+        (6, 6),
+        (6, 7),
+    ]
+
+
+def test_generate_all_adjacent_coordinates():
+    assert generate_all_adjacent_coordinates([(0, 0), (6, 6)], (6, 7)) == [
+        (0, 0),
+        (0, 1),
+        (1, 0),
+        (1, 1),
+        (5, 5),
+        (5, 6),
+        (5, 7),
+        (6, 5),
+        (6, 6),
+        (6, 7),
+    ]
+
+
+def test_get_digit_coordinates():
+    assert get_digit_coordinates(
+        ["..78..", "1../.."], [(0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4)]
+    ) == [(0, 2), (0, 3)]
